756866f451793ef1862b02e0283339daf2fd79be
Wasif Hassan <wasifhassan101@gmail.com>
2024-09-22 01:48:22 +0500

    added logo

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,5 @@
 "use client";
 import React, { useState } from 'react';
-import logoImg from "@/public/logo.png";
-import Image from 'next/image';
 
 const Navbar: React.FC = () => {
     const [menuOpen, setMenuOpen] = useState(false);
